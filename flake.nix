3fceb0a8bc78ce007a455b137bf097ed2df82cb1
Alex Nordin <alex_m_nordin@pm.me>
2024-06-07 03:01:07 +0200

    Change import statement

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
-    esp-idf.url = "github:mirrexagon/nixpkgs-esp-dev/";
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev/";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         # inputs.agenix-shell.flakeModules.default
-        inputs.devenv.flakeModule
+        # inputs.devenv.flakeModule
         # To import a flake module
         # 1. Add foo to inputs
         # 2. Add foo as a parameter to the outputs function
@@ -27,11 +27,20 @@
         lib,
         ...
       }: {
-        devenv.shells.default = {
-          # packages = with pkgs; [
-          # ];
-          packages = with pkgs; [
-            esp32c3-idf
+        devShells.default = pkgs.mkShell {
+          # packages = with pkgs; [sqlx-cli];
+          inputsFrom = [
+            # config.treefmt.build.devShell
+            # self'.devShells.webbed_site
+            # inputs'.nixpkgs-esp-dev.esp32c3-idf
+            # inputs.esp-idf.devShells.esp32c3-idf
+          ];
+          # let
+          # pkgs = import <nixpkgs> { overlays = [ (import "${esp-dev}/overlay.nix") ]; };
+          # in
+          nativeBuildInputs = with pkgs; [
+            just
+            inputs'.nixpkgs-esp-dev.sp32c3-idf
             espflash
           ];
         };
