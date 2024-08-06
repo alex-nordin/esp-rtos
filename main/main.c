e8412fcef36cc29a27b6f4fa0fcbf1cef6e8904e
Alex Nordin <alex_m_nordin@pm.me>
2024-08-07 00:54:27 +0200

    Add wifi station functionality

--- main/main.c
+++ main/main.c
@@ -1,15 +1,8 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
-// #include "esp_chip_info.h"
 #include "esp_event.h"
-#include "esp_flash.h"
 #include "esp_log.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
+#include "freertos/projdefs.h"
 #include "nvs_flash.h"
 
 #include "freertos/FreeRTOS.h"
@@ -17,132 +10,134 @@
 #include "freertos/semphr.h"
 #include "freertos/task.h"
 
-#include "sdkconfig.h"
-#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#define NO_DATA_TIMEOUT_SEC 10
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
-static const char* TAG = "WEBSOCKET";
+#define WIFI_SSID CONFIG_ESP_WIFI_SSID
+#define WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
+#define MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
 
-static TimerHandle_t shutdown_signal_timer;
-static SemaphoreHandle_t shutdown_sema;
+#define WIFI_CONNECTED_BIT 0x01
+#define WIFI_FAIL_BIT 0x02
 
-static void
-shutdown_signaler(TimerHandle_t xTimer)
-{
-    ESP_LOGI(TAG,
-             "No data received for %d seconds, signaling shutdown",
-             NO_DATA_TIMEOUT_SEC);
-    xSemaphoreGive(shutdown_sema);
-}
+/* The handle of the task that will receive notifications from the
+   interrupts. The handle was obtained when the task was created. */
 
-static void
-websocket_event_handler(void* handler_args,
-                        esp_event_base_t base,
-                        int32_t event_id,
-                        void* event_data)
-{
-    esp_websocket_event_data_t* data = (esp_websocket_event_data_t*)event_data;
-    switch (event_id) {
-        case WEBSOCKET_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "WEBSOCKET_EVENT_CONNECTED");
-            break;
-        case WEBSOCKET_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "WEBSOCKET_EVENT_DISCONNECTED");
-            break;
-        case WEBSOCKET_EVENT_DATA:
-            ESP_LOGI(TAG, "WEBSOCKET_EVENT_DATA");
-            ESP_LOGI(TAG, "Received opcode=%d", data->op_code);
-            ESP_LOGW(
-              TAG, "Received=%.*s", data->data_len, (char*)data->data_ptr);
-            ESP_LOGW(TAG,
-                     "Total payload length=%d, data_len=%d, current payload "
-                     "offset=%d\r\n",
-                     data->payload_len,
-                     data->data_len,
-                     data->payload_offset);
+static TaskHandle_t wifi_task_handle;
 
-            xTimerReset(shutdown_signal_timer, portMAX_DELAY);
-            break;
-        case WEBSOCKET_EVENT_ERROR:
-            ESP_LOGI(TAG, "WEBSOCKET_EVENT_ERROR");
-            break;
-    }
-}
+static uint8_t retry_num = 0;
+
+static const char* TAG = "wifi sta";
 
 static void
-websocket_app_start(void)
+event_handler(void* arg,
+              esp_event_base_t event_base,
+              int32_t event_id,
+              void* event_data)
 {
-    esp_websocket_client_config_t websocket_cfg = {};
-
-    shutdown_signal_timer =
-      xTimerCreate("Websocket shutdown timer",
-                   NO_DATA_TIMEOUT_SEC * 1000 / portTICK_PERIOD_MS,
-                   pdFALSE,
-                   NULL,
-                   shutdown_signaler);
-    shutdown_sema = xSemaphoreCreateBinary();
-
-#if CONFIG_WEBSOCKET_URI_FROM_STDIN
-    char line[128];
-
-    ESP_LOGI(TAG, "Please enter uri of websocket endpoint");
-    get_string(line, sizeof(line));
-
-    websocket_cfg.uri = line;
-    ESP_LOGI(TAG, "Endpoint uri: %s\n", line);
-
-#else
-    websocket_cfg.uri = CONFIG_WEBSOCKET_URI;
-
-#endif /* CONFIG_WEBSOCKET_URI_FROM_STDIN */
-
-    ESP_LOGI(TAG, "Connecting to %s...", websocket_cfg.uri);
-
-    esp_websocket_client_handle_t client =
-      esp_websocket_client_init(&websocket_cfg);
-    esp_websocket_register_events(
-      client, WEBSOCKET_EVENT_ANY, websocket_event_handler, (void*)client);
-
-    esp_websocket_client_start(client);
-    xTimerStart(shutdown_signal_timer, portMAX_DELAY);
-    char data[32];
-    int i = 0;
-    while (i < 10) {
-        if (esp_websocket_client_is_connected(client)) {
-            int len = sprintf(data, "hello %04d", i++);
-            ESP_LOGI(TAG, "Sending %s", data);
-            esp_websocket_client_send_text(client, data, len, portMAX_DELAY);
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT &&
+               event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xTaskNotify(wifi_task_handle, WIFI_FAIL_BIT, eSetBits);
         }
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        ESP_LOGI(TAG, "connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        retry_num = 0;
+        xTaskNotify(wifi_task_handle, WIFI_CONNECTED_BIT, eSetBits);
     }
+}
 
-    xSemaphoreTake(shutdown_sema, portMAX_DELAY);
-    esp_websocket_client_stop(client);
-    ESP_LOGI(TAG, "Websocket Stopped");
-    esp_websocket_client_destroy(client);
+void
+wifi_init_sta(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT)
+     * or connection failed for the maximum number of re-tries (WIFI_FAIL_BIT).
+     * The bits are set by event_handler() (see above) */
+    const TickType_t xMaxBlockTime = pdMS_TO_TICKS(500);
+    uint32_t val_from_task;
+    BaseType_t xResult =
+      xTaskNotifyWait(pdFALSE, ULONG_MAX, &val_from_task, xMaxBlockTime);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+     * can test which event actually happened. */
+    if (xResult) {
+        if (val_from_task & WIFI_CONNECTED_BIT) {
+            ESP_LOGI(
+              TAG, "connected to ap SSID:%s password:%s", WIFI_SSID, WIFI_PASS);
+        } else if (val_from_task & WIFI_FAIL_BIT) {
+            ESP_LOGI(TAG,
+                     "Failed to connect to SSID:%s, password:%s",
+                     WIFI_SSID,
+                     WIFI_PASS);
+        } else {
+            ESP_LOGE(TAG, "UNEXPECTED EVENT");
+        }
+    } else {
+        ESP_LOGI(TAG, "timed out waiting for task");
+    }
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
 }
 
 void
 app_main(void)
 {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("WEBSOCKET_CLIENT", ESP_LOG_DEBUG);
-    esp_log_level_set("TRANS_TCP", ESP_LOG_DEBUG);
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in
-     * menuconfig. Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
-
-    websocket_app_start();
+    // return EXIT_SUCCESS;
 }
